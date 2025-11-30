43a762c3e46ae8a5b71b9eed116bafac7fa2f2a2
Seal <ml.muellauer@aon.at>
2025-11-30 01:44:46 +0100

    im menu wird gegrubert

--- scenes/miniscreen_controller.gd
+++ scenes/miniscreen_controller.gd
@@ -1,12 +1,14 @@
 extends Control
 
 @export var emitter_path: NodePath
-
+@export var zoomoutanim: AnimationPlayer
 func _ready():
 	var emitter = get_node(emitter_path)
 	emitter.connect("zoomin", _on_my_signal)
 
-
 func _on_my_signal():
 	print("Signal received")
 	$Placeholder.hide()
+
+func _on_button_pressed() -> void:
+		zoomoutanim.play("zoomback") #es grubert GANZ schlimm
